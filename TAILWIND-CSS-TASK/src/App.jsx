8d82f92c3d83599f170ac044d10d626bb0713f77
cseMisbah <mijbahsakib@gmial.com>
2025-05-24 13:03:33 +0600

    practice_01 done

---
+++ TAILWIND-CSS-TASK/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Practice_01 from "./PRACTICE/Practice_01";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <Practice_01></Practice_01>
+    </div>
+  );
+}
+
+export default App;
---
+++ TAILWIND-CSS-TASK/src/PRACTICE/Practice_01.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Practice_01 = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="bg-red-600 text-white">Hello Bangladesh</h1>
+      </div>
+      <div className="w-60 h-60 bg-black text-white">Hello World</div>
+    </div>
+  );
+};
+
+export default Practice_01;
